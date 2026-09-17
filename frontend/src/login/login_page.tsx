75d6c58d053c6d21303d1b48e16da40a24971bcd
Lucky-luke-ur <lucasherrigel2@gmail.com>
2026-09-17 20:02:28 +0200

    work on front end

--- frontend/src/login/login_page.tsx
+++ frontend/src/login/login_page.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
+
+
+
 function LoginPage() {
+    const [role, setRole] = useState("student");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    if(role == "student"){
+        //Logik hinzufügen bei ZEIT 
+    }
+    if(role == "professor"){
+        //Logik einfügen 
+    }
     return (
         <div>
             <h1>Login Page</h1>
+            <button onClick={() => setRole("student")}>Student</button>
+            <button onClick={() => setRole("professor")}>Professor</button>
             <label>Email:</label>
-            <input type= "email" placeholder="ihre E-Mail"/>
+            <input type= "email" placeholder="ihre E-Mail"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}/>
             <label>Passwort:</label>
-            <input type= "password" placeholder="password eingeben "/>
-            
+            <input type= "password" placeholder="password eingeben "
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}/>
+            {/*überprüfung des States später entfernen  */}
+            <p>{role}</p>
             <button>Login</button>
         </div>
     );
