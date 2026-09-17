3e33c0489f27c501bb4b4bd6428d08f68cdff930
Lucky-luke-ur <lucasherrigel2@gmail.com>
2026-09-17 18:23:27 +0200

    Field for Email, Password an Login button

--- frontend/src/login/login_page.tsx
+++ frontend/src/login/login_page.tsx
@@ -4,6 +4,10 @@ function LoginPage() {
             <h1>Login Page</h1>
             <label>Email:</label>
             <input type= "email" placeholder="ihre E-Mail"/>
+            <label>Passwort:</label>
+            <input type= "password" placeholder="password eingeben "/>
+            
+            <button>Login</button>
         </div>
     );
 }
