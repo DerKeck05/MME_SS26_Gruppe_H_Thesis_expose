24c4c0ef8bfb4cd53b45cbea0609c75d8de98a2a
Lucky-luke-ur <lucasherrigel2@gmail.com>
2026-09-21 00:20:03 +0200

    comments on Code register Page

--- frontend/src/login/register_page.tsx
+++ frontend/src/login/register_page.tsx
@@ -3,21 +3,25 @@ import "./design_css/login.css";
 
 
 function RegisterPage() {
+      /* Zum navigieren auf andere Seite */
     const navigate = useNavigate();
 
     return (
+        /* auth-page für Seitenbereich */
         <div className="auth-page">
-
+              {/* luiqid class box*/}
             <div className="login-glass">
 
                 <h1>Registrieren</h1>
 
                 <p>Bitte eine der folgenden Rollen auswählen</p>
-
+                  {/* Wenn mann auf Professor klickt
+                    wird man zur Professor Registrierung weitergeleitet */}
                 <button onClick={() => navigate("/register/professor")}>
                     Professor
                 </button>
-
+                 {/* Wenn mann auf Student klickt
+                    wird man zur Student Registrierung weitergeleitet */}
                 <button onClick={() => navigate("/register/student")}>
                     Student
                 </button>
@@ -27,5 +31,6 @@ function RegisterPage() {
         </div>
     );
 }
-
+/* Dadurch können wir die RegisterPage
+   in App.tsx importieren und benutzen */
 export default RegisterPage;
