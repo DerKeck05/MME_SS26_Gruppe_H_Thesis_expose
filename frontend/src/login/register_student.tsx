e05b18ba5727511223618000479dc222735a2421
Lucky-luke-ur <lucasherrigel2@gmail.com>
2026-09-20 17:18:45 +0200

    working on register button taht send the data to the backend

--- frontend/src/login/register_student.tsx
+++ frontend/src/login/register_student.tsx
@@ -8,8 +8,14 @@ function RegisterStudentPage() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-     const [course, setCourse] = useState("");
+    const [course, setCourse] = useState("");
 
+    function RegisterFunction() {
+    console.log(name);
+    console.log(email);
+    console.log(password);
+    console.log(course);
+    }
     return (
         <div>
             <h1>Student registrieren</h1>
@@ -20,22 +26,25 @@ function RegisterStudentPage() {
                 value={name}
                 onChange={(event) => setName(event.target.value)}
             />
+            <label>Email</label>
            <input
                 type="email"
                 value={email}
                 onChange={(event) => setEmail(event.target.value)}
             />
+            <label>Password</label>
             <input
                 type="password"
                 value={password}
                 onChange={(event) => setPassword(event.target.value)}
             />
+            <label>Kurs</label>
             <input
                 type="text"
                 value={course}
                 onChange={(event) => setCourse(event.target.value)}
             />
-
+            <button onClick={RegisterFunction}>Registrieren</button>
 
         </div>
     );
