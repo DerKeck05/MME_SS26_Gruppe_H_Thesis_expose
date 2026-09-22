020760adc2b00f10223f8fe6c1ec30f8ccce11dc
Lucky-luke-ur <lucasherrigel2@gmail.com>
2026-09-22 23:31:32 +0200

    firsdt table

--- frontend/src/prof_pages/prof_starpage.tsx
+++ frontend/src/prof_pages/prof_starpage.tsx
@@ -1,5 +1,30 @@
+
+
+
+
+
+
+
+
+
 function ProfStartpage() {
-  return <h1>Professor Dashboard</h1>;
+  return (
+  <main>
+    <div>
+        <h1>Professor Dashboard</h1>
+        <table>
+            <thead>
+                <tr>
+                <th> Name</th>
+                <th> E-Mail</th>
+                <th>Kurs</th>
+                <th>Thesis</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+  </main>
+  );
 }
 
 export default ProfStartpage;
