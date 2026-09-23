e89d6a40a6dd23023317de0061bd54262676b6d2
Lucky-luke-ur <lucasherrigel2@gmail.com>
2026-09-24 00:24:13 +0200

    connect back end to front end

--- frontend/src/prof_pages/prof_starpage.tsx
+++ frontend/src/prof_pages/prof_starpage.tsx
@@ -11,64 +11,66 @@ type Student = {
 
 function ProfStartpage() {
 
-const [showThesisModal, setShowThesisModal] = useState(false);
-const [students, setStudents] = useState<Student[]>([]);
+    const [showThesisModal, setShowThesisModal] = useState(false);
+    const [students, setStudents] = useState<Student[]>([]);
 
 
-useEffect(() => {
-    fetch("http://localhost:3000/api/students")
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-        });
-}, []);
+    useEffect(() => {
+        fetch("http://localhost:3000/api/students")
+            .then((response) => response.json())
+            .then((data) => {
+                setStudents(data);
+            });
+    }, []);
 
-function assignThesis(){
-    setShowThesisModal(true);
-}
+    function assignThesis() {
+        setShowThesisModal(true);
+    }
 
-console.log(showThesisModal);
-let thesisModal = null;
+    console.log(showThesisModal);
+    let thesisModal = null;
 
-if (showThesisModal == true) {
-    thesisModal = (
-    <div className="modal-background">
-    <div className="modal-box">
-        Thesis Popup
-    </div>
-</div>
+    if (showThesisModal == true) {
+        thesisModal = (
+            <div className="modal-background">
+                <div className="modal-box">
+                    Thesis Popup
+                </div>
+            </div>
+        );
+    }
+    return (
+        <main>
+            {thesisModal}
+            <div>
+                <h1>Professor Dashboard</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th> Name</th>
+                            <th> E-Mail</th>
+                            <th>Kurs</th>
+                            <th>Thesis</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {students.map((student) => (
+                            <tr key={student.id}>
+                                <td>{student.name}</td>
+                                <td>{student.email}</td>
+                                <td>{student.course}</td>
+                                <td>
+                                    <button onClick={assignThesis}>
+                                        Thesis Zuordnen
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </main>
     );
-}
-  return (
-  <main>
-    {thesisModal}
-    <div>
-        <h1>Professor Dashboard</h1>
-        <table>
-            <thead>
-                <tr>
-                <th> Name</th>
-                <th> E-Mail</th>
-                <th>Kurs</th>
-                <th>Thesis</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td> maaaa</td>
-                    <td> jfdjjfej</td>
-                    <td>fefffefe</td>
-                    <td>
-                        <button onClick={assignThesis}> 
-                            Thesis Zuordnen 
-                        </button>
-                    </td>
-                </tr>   
-            </tbody>
-        </table>
-    </div>
-  </main>
-  );
 }
 
 export default ProfStartpage;
